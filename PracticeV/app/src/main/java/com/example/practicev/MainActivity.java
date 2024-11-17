c6aa2bc4bdc6326b2146778011cb5e3d3391c7a7
jrscott25 <>
2024-11-17 18:18:03 -0500

    Made some changes. I think it's good now!

--- PracticeV/app/src/main/java/com/example/practicev/MainActivity.java
+++ PracticeV/app/src/main/java/com/example/practicev/MainActivity.java
@@ -12,6 +12,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -27,6 +28,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        getSupportActionBar().setTitle("House I grew up in");
+
         binding.fab.setOnClickListener(view ->
                 Toast.makeText(MainActivity.this, "FAB Clicked!", Toast.LENGTH_SHORT).show()
         );
@@ -36,12 +39,28 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         if (mapFragment != null) {
             mapFragment.getMapAsync(this);
         }
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                Toast.makeText(MainActivity.this, "Home clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (item.getItemId() == R.id.nav_map) {
+                Toast.makeText(MainActivity.this, "Map clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (item.getItemId() == R.id.nav_settings) {
+                Toast.makeText(MainActivity.this, "Settings clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            } else {
+                return false;
+            }
+        });
     }
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        LatLng location = new LatLng(40.7128, -74.0060);
-        googleMap.addMarker(new MarkerOptions().position(location).title("Marker in NYC"));
+        LatLng location = new LatLng(38.78176386961602, -76.8892122745587);  // Coordinates of the house
+        googleMap.addMarker(new MarkerOptions().position(location).title("House I grew up in!"));
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 10f));
     }
 }
